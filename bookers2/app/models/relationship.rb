99a956fe9741ecd0ca1c7368601596409472b613
yonePC <ys.mailaddress0929@gmail.com>
2021-07-04 23:34:28 +0000

    モデルとアソシエーション設定

---
+++ bookers2/app/models/relationship.rb
@@ -0,0 +1,6 @@
+class Relationship < ApplicationRecord
+    belongs_to :user
+    belongs_to :follower, class_name: 'User'
+    
+    
+end
--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+  
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy 
+  has_many :followers, through: :passive_relationships, source: :user 
+  
 end
